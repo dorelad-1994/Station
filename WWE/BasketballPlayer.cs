decc29960e66e6c0074c6ef839283e9144acc1df
dorelad-1994 <dor0@post.bgu.ac.il>
2020-08-05 17:14:23 +0300

    d

---
+++ WWE/BasketballPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WWE
+{
+    class BasketballPlayer
+    {
+        private double _height;
+        private bool _onCourt;
+        public BasketballPlayer(double height,bool onCourt)
+        {
+            _height = height;
+            _onCourt = onCourt;
+        }
+    }  
+}
