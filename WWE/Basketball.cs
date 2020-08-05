a6afbe3ae956ea5dfa3da8ad78a5a340d74cd160
dorelad-1994 <dor0@post.bgu.ac.il>
2020-08-05 17:39:16 +0300

    d

---
+++ WWE/Basketball.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WWE
+{
+    class Basketball
+    {
+        private int _size;
+        
+        public Basketball(int size)
+        {
+            _size = size;
+        }
+
+        public void ChangeSize(int size)
+        {
+            _size = size;
+        }
+    }
+}
