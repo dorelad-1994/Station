48b22459a4f33538f23e98a4ed2027aee01b8db3
dorelad-1994 <dor0@post.bgu.ac.il>
2020-08-06 11:35:51 +0300

    m

--- WWE/Enemy.cs
+++ WWE/Enemy.cs
@@ -9,10 +9,7 @@ namespace WWE
     class Enemy
     {
         private String _name;
-<<<<<<< HEAD
         private double _healthy;
-=======
->>>>>>> dea38a13585334093384a67c2efde7ff2be56e98
         private bool _isDied;
     }
 }
